1fc5b324fcf41d8dcb30f5f82caabb53069d069c
nomayeah <stealassassin@gmail.com>
2018-11-15 18:09:46 +0900

    test

---
+++ wp-content/plugins/privacyPolicy/site_settings.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: 個人情報保護方針変更
+Plugin URI: http://ametrine-inc.com/plugin
+Description: 個人情報保護方針変更用プラグイン
+Author: Noma
+Version: 1.1
+Author URI: http://ametrine-inc.com
+*/
+ 
+function show_privacyPoricy() {
+    echo '現在テスト中';
+}
