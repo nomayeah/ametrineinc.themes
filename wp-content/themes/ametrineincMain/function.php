fbd61a0e57902a2aa592ee59b3527763962e0456
nomayeah <stealassassin@gmail.com>
2018-11-15 15:34:16 +0900

    custom menu add

---
+++ wp-content/themes/ametrineincMain/function.php
@@ -0,0 +1,12 @@
+add_filter('show_admin_bar', '__return_false');
+
+// admin_menu にフック
+add_action('admin_menu', 'register_custom_menu_page');
+function register_custom_menu_page() {
+  // add_menu_page でカスタムメニューを追加
+  add_menu_page('サイト設定', 'サイト設定', 0, 'site_settings', 'create_custom_menu_page', '', 3);
+}
+function create_custom_menu_page() {
+  // カスタムメニューページを読み込む
+  require TEMPLATEPATH.'/admin/site_settings.php';
+}
