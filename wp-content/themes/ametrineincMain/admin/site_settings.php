df996f60bbe17d5e45da7ba725b92c56b4312192
nomayeah <stealassassin@gmail.com>
2018-11-15 16:07:24 +0900

    privacy menu custom ver1.1

--- wp-content/themes/ametrineincMain/admin/site_settings.php
+++ wp-content/themes/ametrineincMain/admin/site_settings.php
@@ -1,1 +1,5 @@
-test
+<?php
+function my_custom_menu_page(){
+  echo "<h2>個人情報変更</h2>";   
+  echo "現在テスト中";
+}
--- wp-content/themes/ametrineincMain/functions.php
+++ wp-content/themes/ametrineincMain/functions.php
@@ -2,12 +2,12 @@
 add_filter('show_admin_bar', '__return_false');
 
 // admin_menu にフック
-add_action('admin_menu', 'register_custom_menu_page');
-function register_custom_menu_page() {
+add_action('admin_menu', 'personal_information_menu');
+function personal_information_menu() {
   // add_menu_page でカスタムメニューを追加
-  add_menu_page('サイト設定', 'サイト設定', 0, 'site_settings', 'create_custom_menu_page', '', 3);
+  add_menu_page('個人情報保護方針', '個人情報保護方針', 0, 'site_settings', 'personal_information_create', '', 3);
 }
-function create_custom_menu_page() {
+function personal_information_create() {
   // カスタムメニューページを読み込む
   require TEMPLATEPATH.'/admin/site_settings.php';
 }
