e2e30bf43fe5c9bc9767b724e7522114208e819d
nomayeah <stealassassin@gmail.com>
2018-11-15 18:31:47 +0900

    test

--- wp-content/plugins/privacyPolicy/site_settings.php
+++ wp-content/plugins/privacyPolicy/site_settings.php
@@ -12,6 +12,11 @@ class showPrivacyPoricy {
   function __construct() {
     add_action('admin_menu', array($this, 'add_pages'));
   }
-
+  function add_pages() {
+    add_menu_page('個人情報保護方針編集', '個人情報保護方針編集', 'level_8', __FILE__, array($this,'show_text_option_page'), '', 26);
+  }
+  function show_text_option_page() {
+    
+  }
 }
 $showtext = new showPrivacyPoricy;
