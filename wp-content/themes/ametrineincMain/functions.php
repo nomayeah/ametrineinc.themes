09b58c8e32c8bd639667ce4bff3bc450bb6b2dba
nomayeah <stealassassin@gmail.com>
2018-11-27 17:35:59 +0900

    test

--- wp-content/themes/ametrineincMain/functions.php
+++ wp-content/themes/ametrineincMain/functions.php
@@ -1,2 +1,21 @@
 <?php
 add_filter('show_admin_bar', '__return_false');
+
+
+function footerScripts() {
+  wp_enqueue_script( 
+    'jquery3.0', 
+    get_template_directory_uri() . '/js/jquery3.0.0.js',
+    array(),
+    false,
+    true
+  );
+  wp_enqueue_script( 
+    'bundle', 
+    get_template_directory_uri() . '/js/bundle.js',
+    array( 'jquery3.0' ),
+    false,
+    true
+  );
+}
+add_action( 'wp_enqueue_scripts', 'footerScripts' );
---
+++ wp-content/themes/ametrineincMain/js/bundle.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(document).ready(function(){
+    var wH = $(window).height(),
+        target1 = $('.js-scrollLogo__parts1');
+
+    $(target1).css('height',wH+'px');
+  });
+});
