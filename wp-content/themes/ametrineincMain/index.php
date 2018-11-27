34eece64049881f1e8328348dc8af9db4ba50a82
nomayeah <stealassassin@gmail.com>
2018-11-27 18:16:39 +0900

    js test

--- wp-content/themes/ametrineincMain/index.php
+++ wp-content/themes/ametrineincMain/index.php
@@ -2,11 +2,15 @@
 <?php get_header(); ?>
 
 <section class="js-scrollLogo__parts1">
-  <img src="/test/wp-content/uploads/2018/11/testlogo.png" alt="">
+  <div class="js-sectionLogo__partsTop"></div>
+  <div><img src="/test/wp-content/uploads/2018/11/testlogo.png" alt=""></div>
+  <div class="js-sectionLogo__partsBottom"></div>
 </section>
+
 <header>
   
 </header>
+
 <main class="js-scrollLogo__parts2">
 <div class="loop_css">
   <img src="/test/wp-content/uploads/2018/11/bg_img.png"><img src="/test/wp-content/uploads/2018/11/bg_img.png">
