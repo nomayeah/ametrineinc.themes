596bf770677280c1a2afb5d78c5254c083c2afb1
nomayeah <stealassassin@gmail.com>
2018-11-27 17:55:35 +0900

    js test

--- wp-content/themes/ametrineincMain/js/bundle.js
+++ wp-content/themes/ametrineincMain/js/bundle.js
@@ -1,6 +1,6 @@
 $(function(){
   $(document).ready(function(){
-    var wH = $(window).height(),
+    var wH = innerHeight,
         target1 = $('.js-scrollLogo__parts1');
 
     $(target1).css('height',wH+'px');
