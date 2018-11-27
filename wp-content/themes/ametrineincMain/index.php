2c7b71b47a3d19d3b662d5cbb46489454e49cec7
nomayeah <stealassassin@gmail.com>
2018-11-27 16:28:33 +0900

    upload

--- wp-content/themes/ametrineincMain/index.php
+++ wp-content/themes/ametrineincMain/index.php
@@ -1,6 +1,14 @@
 
 <?php get_header(); ?>
-  <body>
-    body
-  </body>
+<body>
+  <section>
+    <img src="/test/wp-content/uploads/2018/11/testlogo.png" alt="">
+  </section>
+  <header>
+    
+  </header>
+  <main>
+    <img src="/test/wp-content/uploads/2018/11/bg_img.png" alt="">
+  </main>
+</body>
 <?php get_footer(); ?>
