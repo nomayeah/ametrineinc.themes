cbde0889ec631c670e2fbdfdc27bf63810202386
zap-ynonaka <y_nonaka@zappallas.com>
2018-12-06 15:37:40 +0900

    test

--- wp-content/themes/ametrineincMain/functions.php
+++ wp-content/themes/ametrineincMain/functions.php
@@ -1,21 +1,30 @@
 <?php
 add_filter('show_admin_bar', '__return_false');
 
-
+// js読み込み
 function footerScripts() {
   wp_enqueue_script( 
-    'jquery3.0', 
-    get_template_directory_uri() . '/js/jquery3.0.0.js',
+    'bundle', 
+    get_template_directory_uri() . '/dist/bundle.js',
     array(),
     false,
     true
   );
-  wp_enqueue_script( 
-    'bundle', 
-    get_template_directory_uri() . '/js/bundle.js',
-    array( 'jquery3.0' ),
-    false,
-    true
-  );
+  // wp_enqueue_script( 
+  //   'test', 
+  //   get_template_directory_uri() . '/js/test.js',
+  //   array( 'test' ),
+  //   false,
+  //   true
+  // );
 }
 add_action( 'wp_enqueue_scripts', 'footerScripts' );
+
+// css読み込み
+function styleCss() {
+  wp_enqueue_style(
+    'main-customStyle',
+    get_template_directory_uri() . '/dist/style.css'
+  );
+}
+add_action( 'wp_enqueue_scripts', 'styleCss' );
