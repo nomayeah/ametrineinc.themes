1e331c0d75325fc4f31d9b4f3383d49b1cf9c4c2
zap-ynonaka <y_nonaka@zappallas.com>
2018-12-06 17:51:14 +0900

    test

---
+++ wp-content/themes/ametrineincMain/front-page.php
@@ -0,0 +1,27 @@
+
+<?php get_header(); ?>
+
+<header class="js-scrollLogo__parts1">
+  <div class="js-sectionLogo__partsTop"></div>
+  <h1><img src="/test/wp-content/uploads/2018/11/testlogo.png" alt="Ametrine.incのサイトロゴ"></h1>
+  <div class="js-sectionLogo__partsBottom"></div>
+</header>
+
+<main class="js-scrollLogo__parts2">
+  <div class="loop-css">
+    <img src="/test/wp-content/uploads/2018/11/bg_img.png"><img src="/test/wp-content/uploads/2018/11/bg_img.png">
+    <div class="loop-message"><span>AMETRINEは</span><span>”繋ぎ”をつくる</span></div>
+  </div>
+
+  <section class="top-sec__standard">
+    <h2>セクションタイトル</h2>
+    <div class="top-sec__block">
+      <h3>タイトルタイトル</h3>
+      <div class="top-sec__img"><img src="/test/wp-content/uploads/2018/12/test_topimg1.png" alt=""></div>
+      <div class="top-sec__caption">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</div>
+      <div class="top-sec__btn"><a href="#"><img src="/test/wp-content/uploads/2018/12/test_btn1.png" alt=""></a></div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
