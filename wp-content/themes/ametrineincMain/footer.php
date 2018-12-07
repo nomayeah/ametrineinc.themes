482ac83c5d438dff53b0df59df2d2ce3306f7e15
zap-ynonaka <y_nonaka@zappallas.com>
2018-12-07 15:00:03 +0900

    test

--- wp-content/themes/ametrineincMain/footer.php
+++ wp-content/themes/ametrineincMain/footer.php
@@ -1,11 +1,7 @@
     <footer>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      footer
+
+      <div class="footer">AMETRINE All rights reserved.</div>
     </footer>
     <?php wp_footer(); ?>
-
   </body>
 </html>
