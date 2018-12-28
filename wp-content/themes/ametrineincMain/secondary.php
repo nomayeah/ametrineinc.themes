a48879cb187cf0eac841b6ae1d3ddbf6d42a12b6
zap-ynonaka <y_nonaka@zappallas.com>
2018-12-28 17:32:15 +0900

    2階層目

---
+++ wp-content/themes/ametrineincMain/secondary.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: ２階層目ベース
+ */
+get_header(); ?>
+<body>
+<div class="js-pageOpen"></div>
+<h1><?php the_title(); ?></h1>
+<section>
+  <?php the_content(); ?>
+</section>
+</body>
+<?php get_footer(); ?>
