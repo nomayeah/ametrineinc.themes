835eb4fe057e0eac1cb12b7591aae25bea69dcca
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-03 00:52:54 +0900

    second着手フォームcss調整

--- wp-content/themes/ametrineincMain/page-secondary.php
+++ wp-content/themes/ametrineincMain/page-secondary.php
@@ -13,9 +13,14 @@ get_header(); ?>
 </div>
 
 <section class="sec-body">
-  <?php if(have_posts()): while(have_posts()): the_post(); ?>
-    <?php the_content(); ?>
-  <?php endwhile; endif; ?>
+<?php 
+  if (have_posts()):
+    while (have_posts()) :
+      the_post();
+      the_content();
+    endwhile;
+  endif;
+?>
 </section>
 
 <?php
