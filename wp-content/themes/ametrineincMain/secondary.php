403d55fc398be8919e2bee6854105e68e497d103
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-02 18:41:42 +0900

    the_content修正

--- wp-content/themes/ametrineincMain/secondary.php
+++ wp-content/themes/ametrineincMain/secondary.php
@@ -13,7 +13,9 @@ get_header(); ?>
 </div>
 
 <section class="sec-body">
-  <?php the_content(); ?>
+  <?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <?php the_content(); ?>
+  <?php endwhile; endif; ?>
 </section>
 
 <?php
