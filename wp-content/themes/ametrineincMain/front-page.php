a9e129385c465cf12ff579d9d9c6d23c28c1fc09
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-06 13:12:55 +0900

    top修正

--- wp-content/themes/ametrineincMain/front-page.php
+++ wp-content/themes/ametrineincMain/front-page.php
@@ -4,7 +4,7 @@
   <div class="js-sectionLogo__partsTop"></div>
   <h1 class="top-logo">
     <span class="js-sectionLogo__partsLeft"></span>
-    <img src="<?php bloginfo('template_directory'); ?>/images/top/topLogoSP.png" class="device-sp" alt="Ametrine.incのサイトロゴ">
+    <img src="<?php bloginfo('template_directory'); ?>/images/top/topLogo.png" alt="Ametrine.incのサイトロゴ">
     <span class="js-sectionLogo__partsRight"></span>
     </h1>
   <div class="js-sectionLogo__partsBottom"></div>
