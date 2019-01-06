6f47cccf547a866d3bb537e2e3248f89ffde5075
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-06 16:00:34 +0900

    二階層目修正

--- wp-content/themes/ametrineincMain/page-secondary.php
+++ wp-content/themes/ametrineincMain/page-secondary.php
@@ -18,17 +18,9 @@ get_header(); ?>
 
 <?php $page_id = 48; $content = get_page($page_id); echo $content -> post_content; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php
-    // ポスト情報の取得
-    $ID = $post->ID; // 投稿ID
-    $title = $post->post_title; // タイトル
-    $content = $post->post_content; // 投稿内容
-    $slug = $post->post_name; // スラッグ
-    $parent = $post->post_parent; // 親投稿の有無
-    $uri = get_page_uri($ID);
-  ?>
-  <?php echo( $post->ID ); ?> <?php echo( $ID ); ?>
-<?php endwhile; ?>bbb
+<script>
+  var page_id = <?php while (have_posts()): the_post(); ?><?php echo( $post->ID ); ?><?php endwhile; ?>;
+  console.log(page_id);
+</script>
 
 <?php get_footer(); ?>
