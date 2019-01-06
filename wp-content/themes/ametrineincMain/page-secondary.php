c15c3a17fe2fe856e7532558f9a773c4dc30ae08
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-06 15:48:43 +0900

    二階層目修正

--- wp-content/themes/ametrineincMain/page-secondary.php
+++ wp-content/themes/ametrineincMain/page-secondary.php
@@ -18,6 +18,6 @@ get_header(); ?>
 
 <?php $page_id = 48; $content = get_page($page_id); echo $content -> post_content; ?>
 
-<?php get_the_ID(); ?>
+<?php get_the_ID(); ?>aaa
 
 <?php get_footer(); ?>
