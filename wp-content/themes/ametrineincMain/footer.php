e1b7aca0adb40ed0a3e5a9a745bf415151059b37
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-26 15:14:23 +0900

    スクロール修正1-

--- wp-content/themes/ametrineincMain/footer.php
+++ wp-content/themes/ametrineincMain/footer.php
@@ -3,5 +3,31 @@
       <div class="copy">AMETRINE All rights reserved.</div>
     </footer>
     <?php wp_footer(); ?>
+
+    <script>
+    $(window).load(function(){
+      $(function() {
+        $.scrollify({
+          section : "section.test",
+          sectionName : "section-name",
+          interstitialSection : "",
+          easing: "easeOutExpo",
+          scrollSpeed: 1100,
+          offset : 0,
+          scrollbars: true,
+          standardScrollElements: "",
+          setHeights: true,
+          overflowScroll: true,
+          updateHash: true,
+          touchScroll:true,
+          before:function() {},
+          after:function() {},
+          afterResize:function() {},
+          afterRender:function() {}
+        });
+              
+      });
+    });
+    </script>
   </body>
 </html>
