491b258dcaf1c73e1284b077874e61b04cb2bffe
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-26 15:23:00 +0900

    スクロール修正13

--- wp-content/themes/ametrineincMain/dist/jquery2.2.1.js
+++ wp-content/themes/ametrineincMain/dist/jquery2.2.1.js
@@ -816,27 +816,25 @@ $.scrollify = scrollify;
 return scrollify;
 }));
 
-$(window).load(function(){
-  $(function() {
-    $.scrollify({
-      section : "section.test",
-      sectionName : "section-name",
-      interstitialSection : "",
-      easing: "easeOutExpo",
-      scrollSpeed: 1100,
-      offset : 0,
-      scrollbars: true,
-      standardScrollElements: "",
-      setHeights: true,
-      overflowScroll: true,
-      updateHash: true,
-      touchScroll:true,
-      before:function() {},
-      after:function() {},
-      afterResize:function() {},
-      afterRender:function() {}
-    });
-          
+$(function() {
+  $.scrollify({
+    section : "section.test",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
   });
+
   console.log("done");
 });
