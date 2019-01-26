b4e2944c9b6360a92a23054a1457d09df051e76c
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-26 15:26:39 +0900

    スクロール修正15

--- wp-content/themes/ametrineincMain/front-page.php
+++ wp-content/themes/ametrineincMain/front-page.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
+<?php get_header(); ?>
+
 <body>
   <section class="test"><header style="background-color: #fcc;">セクション1</header></section>
   <section class="test" style="background-color: #ccf;">セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br>セクション2<br></section>
