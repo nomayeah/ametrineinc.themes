63dca41f447496a966bd0d7fd7a1291e362fcfdb
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-26 14:52:30 +0900

    スクロール修正8

--- wp-content/themes/ametrineincMain/front-page.php
+++ wp-content/themes/ametrineincMain/front-page.php
@@ -1,31 +1,35 @@
 <?php get_header(); ?>
 
-<header class="js-scrollLogo__parts1">
-  <div class="js-sectionLogo__partsTop"></div>
-  <h1 class="top-logo">
-    <span class="js-sectionLogo__partsLeft"></span>
-    <span class="top-logo__main"><img src="<?php bloginfo('template_directory'); ?>/images/top/topLogo.png" alt="Ametrine.incのサイトロゴ"></span>
-    <span class="js-sectionLogo__partsRight"></span>
-    </h1>
-  <div class="js-sectionLogo__partsBottom"></div>
-</header>
+<section class="js-scrollArea">
+  <header class="js-scrollLogo__parts1">
+    <div class="js-sectionLogo__partsTop"></div>
+    <h1 class="top-logo">
+      <span class="js-sectionLogo__partsLeft"></span>
+      <span class="top-logo__main"><img src="<?php bloginfo('template_directory'); ?>/images/top/topLogo.png" alt="Ametrine.incのサイトロゴ"></span>
+      <span class="js-sectionLogo__partsRight"></span>
+      </h1>
+    <div class="js-sectionLogo__partsBottom"></div>
+  </header>
+</section>
 
-<main class="js-scrollLogo__parts2">
-  <?php $page_id = 74; $content = get_page($page_id); echo $content -> post_content; ?>
+<section class="js-scrollArea">
+  <main class="js-scrollLogo__parts2">
+    <?php $page_id = 74; $content = get_page($page_id); echo $content -> post_content; ?>
 
-  <div class="loop-css">
-    <img src="<?php bloginfo('template_directory'); ?>/images/scroll/scroll.png"><img src="<?php bloginfo('template_directory'); ?>/images/scroll/scroll.png">
-    <div class="loop-message" style="transition: all 0s"><span>AMETRINEは</span><span>”繋ぎ”をつくる</span></div>
-  </div>
-
-  <?php $page_id = 36; $content = get_page($page_id); echo $content->post_content; ?>
-
-  <section class="top-sec__standard co">
-    <div class="top-sec__block">
-      <?php $page_id = 22; $content = get_page($page_id); echo $content->post_content; ?>
+    <div class="loop-css">
+      <img src="<?php bloginfo('template_directory'); ?>/images/scroll/scroll.png"><img src="<?php bloginfo('template_directory'); ?>/images/scroll/scroll.png">
+      <div class="loop-message" style="transition: all 0s"><span>AMETRINEは</span><span>”繋ぎ”をつくる</span></div>
     </div>
-  </section>
 
-</main>
+    <?php $page_id = 36; $content = get_page($page_id); echo $content->post_content; ?>
+
+    <section class="top-sec__standard co">
+      <div class="top-sec__block">
+        <?php $page_id = 22; $content = get_page($page_id); echo $content->post_content; ?>
+      </div>
+    </section>
+
+  </main>
+</section>
 
 <?php get_footer(); ?>
