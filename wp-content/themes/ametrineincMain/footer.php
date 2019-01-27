22e3d263cfd37926f686617d78e7f3eebb40d3dc
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-28 00:02:25 +0900

    トップ修正3

--- wp-content/themes/ametrineincMain/footer.php
+++ wp-content/themes/ametrineincMain/footer.php
@@ -1,14 +1,12 @@
     <footer>
       <div class="ft-title"><img src="<?php bloginfo('template_directory'); ?>/images/top/logoText.png" alt=""></div>
       <ul class="ft-list">
-        <li><a href="/?page_id=122">個人情報保護方針</a></li>
-      </ul>
-      <ul class="ft-list">
         <li><a href="/?page_id=8">企業コンサルティング</a></li>
         <li><a href="/?page_id=79">人材コンサルティング</a></li>
         <li><a href="/?page_id=81">イベント事業</a></li>
         <li><a href="/?page_id=83">企業マッチング・営業代行業</a></li>
         <li><a href="/?page_id=87">地方創生・活性化事業</a></li>
+        <li><a href="/?page_id=122">個人情報保護方針</a></li>
       </ul>
       <div class="copy">AMETRINE All rights reserved.</div>
     </footer>
