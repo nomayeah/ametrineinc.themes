b06e83c5396ae7edb4e46fff5bbfbafd185b85a9
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-27 23:07:05 +0900

    トップ修正

--- wp-content/themes/ametrineincMain/front-page.php
+++ wp-content/themes/ametrineincMain/front-page.php
@@ -15,7 +15,7 @@
 
   <div class="loop-css">
     <img src="<?php bloginfo('template_directory'); ?>/images/scroll/scroll.png"><img src="<?php bloginfo('template_directory'); ?>/images/scroll/scroll.png">
-    <div class="loop-message" style="transition: all 0s"><span>AMETRINEは</span><span>”繋ぎ”をつくる</span></div>
+    <div class="loop-message" style="transition: all 0s"><span>AMETRINEは</span><strong>”繋ぎ”</strong><span>をつくる</span></div>
   </div>
 
   <?php $page_id = 36; $content = get_page($page_id); echo $content->post_content; ?>
