603719fbe08f527ddb00250142b4a7a19a223b5e
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-27 21:47:36 +0900

    footer　しゅうせい

--- wp-content/themes/ametrineincMain/footer.php
+++ wp-content/themes/ametrineincMain/footer.php
@@ -1,5 +1,14 @@
     <footer>
-
+      <div class="ft-title"><img src="" alt=""></div>
+      <ul class="ft-list">
+        <li><a href="/">TOP</a></li>
+        <li><a href="/?page_id=122">個人情報保護方針</a></li>
+        <li><a href="/?page_id=8">企業コンサルティング</a></li>
+        <li><a href="/?page_id=79">人材コンサルティング</a></li>
+        <li><a href="/?page_id=81">イベント事業</a></li>
+        <li><a href="/?page_id=83">企業マッチング・営業代行業</a></li>
+        <li><a href="/?page_id=87">地方創生・活性化事業</a></li>
+      </ul>
       <div class="copy">AMETRINE All rights reserved.</div>
     </footer>
     <?php wp_footer(); ?>
