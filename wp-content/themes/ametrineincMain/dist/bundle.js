3f3fae92c7bb4a185d9a522633bc029f989c5255
zap-ynonaka <y_nonaka@zappallas.com>
2019-01-29 16:58:48 +0900

    bl-list 修正

--- wp-content/themes/ametrineincMain/dist/bundle.js
+++ wp-content/themes/ametrineincMain/dist/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("$(function () {\n  $('.bl-list a').each(function () {\n    var $href = $(this).attr('href').replace('/?page_id=', ''),\n        target1,\n        target2;\n\n    if ($href == page_id) {\n      $(this).parent().addClass('hidden');\n    }\n\n    for (var i = 0; i <= $('.bl-list li').length; i++) {\n      target1 = $('.bl-list li:nth-of-type(' + i + ')');\n\n      if ($(dd).hasClass('hidden')) {\n        var target2 = i - 1;\n        $('.bl-list li:nth-of-type(' + target2 + ')').css({\n          'border-bottom': '0'\n        });\n      } else {\n        return;\n      }\n    }\n  });\n});\n\n//# sourceURL=webpack:///./src/js/businessList/mix.js?");
+eval("$(function () {\n  $('.bl-list a').each(function () {\n    var $href = $(this).attr('href').replace('/?page_id=', ''),\n        target1,\n        target2;\n\n    if ($href == page_id) {\n      $(this).parent().addClass('hidden');\n    }\n  });\n\n  for (var i = 0; i <= $('.bl-list li').length; i++) {\n    target1 = $('.bl-list li:nth-of-type(' + i + ')');\n\n    if ($(dd).hasClass('hidden')) {\n      var target2 = i - 1;\n      $('.bl-list li:nth-of-type(' + target2 + ')').css({\n        'border-bottom': '0'\n      });\n    } else {\n      return;\n    }\n  }\n});\n\n//# sourceURL=webpack:///./src/js/businessList/mix.js?");
 
 /***/ }),
 
