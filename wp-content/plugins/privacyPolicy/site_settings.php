170b532b8b5129b8f40e8b71b4956f2b73cbf3e1
zap-ynonaka <y_nonaka@zappallas.com>
2019-02-04 13:05:04 +0900

    画像アップロード

--- wp-content/plugins/privacyPolicy/site_settings.php
+++ wp-content/plugins/privacyPolicy/site_settings.php
@@ -15,6 +15,8 @@ class showPrivacyPoricy {
   function add_pages() {
     add_menu_page('個人情報保護方針編集', '個人情報保護方針編集', 'level_8', __FILE__, array($this,'show_text_option_page'), '', 26);
   }
+
+
   function show_text_option_page() {
     //$_POST['showtext_options'])があったら保存
     if ( isset($_POST['showtext_options'])) {
@@ -26,7 +28,7 @@ class showPrivacyPoricy {
     }
     ?>
     <div class="wrap">
-    <div id="icon-options-general" class="icon32"><br /></div><h2>個人情報保護方針編集</h2>
+    <h2>個人情報保護方針編集</h2>
       <form action="" method="post">
       <?php
         wp_nonce_field('shoptions');
